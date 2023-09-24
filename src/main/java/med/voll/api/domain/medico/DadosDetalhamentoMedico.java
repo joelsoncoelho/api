4401c39a33a4d1fa50096c6e9308b66e4d56a71f
Joelson Coelho <joelson.save@gmail.com>
2023-09-24 14:29:49 -0300

    tratar  exceptions da API, com a utilização da anotação @RestControllerAdvice
    Co-Authored-By: joelson <3879829+joelson@users.noreply.github.com>

--- src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
@@ -1,12 +1,6 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import med.voll.api.endereco.DadosEndereco;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone,Especialidade especialidade, Endereco endereco) {
 
